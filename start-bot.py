3d47be7c4229a4838068afc5ac0160fe57a3d28c
argwood <argwood@uchicago.edu>
2019-06-24 14:04:12 -0500

    bug fix for error messages

--- start-bot.py
+++ start-bot.py
@@ -9,7 +9,7 @@ client = discord.Client()
 token = ''
 sorting_channel = ''
 
-if token = '' or if sorting_channel = '':
+if token == '' or sorting_channel == '':
     print('Please enter an appropriate bot token and channel ID to begin')
 
 sorting_hat = sortinghat.SortingHat(client)
@@ -22,17 +22,17 @@ async def on_ready():
 async def on_message(message):
     if sorting_channel in message.channel.id:
         if message.content.lower().startswith('!sort'):
-	    await sorting_hat.start(message)
-	if message.content.lower().startswith('!stop'):
-	    await sorting_hat.stop(message.channel)
-	if message.content.lower().startswith('yes'):
-	    await sorting_hat.quiz(message)
-	if message.content.lower().startswith('!start'):
-	    await client.send_message(message.channel, 'Type `!sort` to begin to the Sorting Ceremony')
+            await sorting_hat.start(message)
+        if message.content.lower().startswith('!stop'):
+            await sorting_hat.stop(message.channel)
+        if message.content.lower().startswith('yes'):
+            await sorting_hat.quiz(message)
+        if message.content.lower().startswith('!start'):
+            await client.send_message(message.channel, 'Type `!sort` to begin to the Sorting Ceremony')
 
 if __name__ == "__main__":
 
-	try:
-		client.run(token)
-	except:
-		client.close()
+    try:
+        client.run(token)
+    except:
+        client.close()
